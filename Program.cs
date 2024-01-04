3a137cf1e14c5d2e8092b24de1bc6f3942fd3a9a
Sergey Smirnov <sergey.smirnov.92@gmail.com>
2024-01-04 11:30:06 +0300

    Created code to create array with string elements

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.Write("Введите количество элементов массива:\t");
+int elementsCount = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[elementsCount];
+for (int i=0; i < elementsCount; i++)
+{
+    Console.Write($"\nВведите элемент массива с индексовм {i}:\t");
+    array[i] = Console.ReadLine();
+}
+//Console.WriteLine($"\nМассив: [{String.Join("\t", array)}]");
+
+printArray(array);
+static void printArray (string[] array)
+{
+    Console.WriteLine($"\nМассив: [{String.Join("\t", array)}]");
+}
