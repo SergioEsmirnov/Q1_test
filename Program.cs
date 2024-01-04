2eba4f40173738e37ec87beec64f09724006e101
Sergey Smirnov <sergey.smirnov.92@gmail.com>
2024-01-04 13:16:13 +0300

    Added code to create new array with elements <=3 symbols

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Write("Введите количество элементов массива:\t");
+﻿using System.Globalization;
+using System.Xml.XPath;
+
+Console.Write("Введите количество элементов массива:\t");
 int elementsCount = Convert.ToInt32(Console.ReadLine());
 string[] array = new string[elementsCount];
 for (int i=0; i < elementsCount; i++)
@@ -8,8 +11,39 @@ for (int i=0; i < elementsCount; i++)
 }
 //Console.WriteLine($"\nМассив: [{String.Join("\t", array)}]");
 
+int newArraySize = countNewElements(array);
+
+int countNewElements(string[] array)
+{
+    int elements = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length<=3)
+        {
+            elements++;
+        }
+    }
+    return elements;
+}
+
+
+string[] newArray = new string[newArraySize];
+int j = 0;
+for (int i=0; i < elementsCount; i++)
+{
+        if(array[i].Length<=3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }    
+}
+
+
 printArray(array);
 static void printArray (string[] array)
 {
-    Console.WriteLine($"\nМассив: [{String.Join("\t", array)}]");
+    Console.WriteLine($"\n[{String.Join("\t", array)}]");
 }
+
+Console.Write("Новый массив: ");
+printArray(newArray);
