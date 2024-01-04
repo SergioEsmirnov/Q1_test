2ae2dd5448254b110554256651fff34d90362cdb
Sergey Smirnov <sergey.smirnov.92@gmail.com>
2024-01-04 15:26:26 +0300

    Merged changes from branch method to bramch master

--- Program.cs
+++ Program.cs
@@ -2,10 +2,10 @@
 using System.Xml.XPath;
 
 Console.Write("Введите количество элементов массива:\t");
-int elementsCount = Convert.ToInt32(Console.ReadLine());
+int elementsCount = Convert.ToInt32(Console.ReadLine());//запрашиваем количество элементов в создаваемом массиве
 
 string[] array = getArray(elementsCount);//создаем массив с помощью метода
-string[] getArray(int size)
+string[] getArray(int size) //создаем метод для генерации массива из вводимых значений
 {
     string[] array = new string[size];
     for (int i=0; i < size; i++)
@@ -15,7 +15,7 @@ string[] getArray(int size)
         }
     return array;
 }
-printArray(array);
+printArray(array); //выводим массив из введенных элементов
 
 
 int newArraySize = countNewElements(array); // считаем в массиве 
@@ -35,8 +35,9 @@ int countNewElements(string[] array)
 }
 
 
-string[] newArray = resultArray(array, newArraySize);
-string[] resultArray(string[] array, int size)
+string[] newArray = resultArray(array, newArraySize);//создаем новый массив с элементами длиной не более 3 символов
+string[] resultArray(string[] array, int size) // создаем метод которые генерирует из
+// исходного массива новый массив с элементами длиной не более 3 символов
 {
         string[] newArray = new string[size];
     int j = 0;
@@ -52,10 +53,10 @@ string[] resultArray(string[] array, int size)
 }
 
 
-static void printArray (string[] array)
+static void printArray (string[] array) // создаем метод вывода массива
 {
     Console.WriteLine($"\n[{String.Join("\t", array)}]");
 }
 
 Console.Write("Новый массив: ");
-printArray(newArray);
+printArray(newArray); //выводим массив с элементами длиной не более 3 символов
